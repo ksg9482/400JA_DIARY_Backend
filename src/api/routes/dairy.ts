32f823a55eb1c140c2ab93dd623019b5aabc4461
ksg9482 <ksg930523@gmail.com>
2022-07-29 00:03:56 +0900

    create diary method

--- src/api/routes/dairy.ts
+++ src/api/routes/dairy.ts
@@ -1,9 +1,25 @@
-import { Router } from "express";
+import { Response, Router } from "express";
+import { IattachCurrentUserRequest } from "@/interfaces/IRequest";
 import attachCurrentUser from "../middlewares/attachCurrentUser";
+import { createDiary } from "@/services/diary/diary.factory";
 
 const route = Router();
 
 export default (app:Router) => {
-    app.use('/diary', route)
-    app.use(attachCurrentUser)
+    app.use('/diary',attachCurrentUser, route)
+    
+    route.get('/all', async (req: IattachCurrentUserRequest, res: Response) => {
+        const userId = req.currentUser._id;
+        const diaryServiceInstance = createDiary();
+        const result = await diaryServiceInstance.findAllDiary(userId);
+        return res.status(200).json(result);
+    });
+
+    route.post('/', async (req: IattachCurrentUserRequest, res: Response) => {
+        const userId = req.currentUser._id;
+        const diaryContent = req.body
+        const diaryServiceInstance = createDiary();
+        const result = await diaryServiceInstance.createDiaryContent(userId, diaryContent);
+        return res.status(200).json(result);
+    });
 }
